22a8886cfb6cef7cfd01b62111c48cf6de0e93a0
GuoJingtao-1997 <53637977+GuoJingtao-1997@users.noreply.github.com>
2024-07-30 11:02:06 +0800

    Update csi_forward.sh

--- csi_forward.sh
+++ csi_forward.sh
@@ -14,7 +14,7 @@ nft add table ip nexmon
 nft 'add chain ip nexmon input  { type filter hook input  priority -150; policy accept; }'
 nft 'add chain ip nexmon output { type filter hook output priority  150; policy accept; }'
 
-nft add rule ip nexmon input  iifname "wlan0" ip protocol udp ip saddr 10.10.10.10 ip daddr 255.255.255.255 udp sport 5500 udp dport 5500 counter mark set 900 dup to IP-OF-LAPTOP device "eth0"
-nft add rule ip nexmon output oifname "eth0"  meta mark 900 counter ip saddr set IP-OF-NEXMON-DEVICE ip daddr set IP-OF-LAPTOP
+nft add rule ip nexmon input  iifname "wlan0" ip protocol udp ip saddr 10.10.10.10 ip daddr 255.255.255.255 udp sport 5500 udp dport 5500 counter mark set 900 dup to $2 device "eth0"
+nft add rule ip nexmon output oifname "eth0"  meta mark 900 counter ip saddr set $1 ip daddr set $2
 
 echo "CSI Forwarding rules added"
