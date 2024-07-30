065e3088b8c40603a5508e3bae4a0b86ad9c840d
GuoJingtao-1997 <53637977+GuoJingtao-1997@users.noreply.github.com>
2024-07-30 12:47:23 +0800

    Update generate_traffic.sh

--- generate_traffic.sh
+++ generate_traffic.sh
@@ -4,9 +4,6 @@
  # @Date: 2022-04-03 15:09:07
  # @LastEditTime: 2022-12-18 18:21:19
  # @LastEditors: Guo Jingtao
- # @Description: 
- # @FilePath: /undefined/Users/guojingtao/Documents/pi_config_files/generate_traffic.sh
- # 
 ### 
 
 Fs="$(echo "1 / $1" | bc -l)"
